eaf0539c41338017953d65bc6d9d5ffb57a72f86
Anna <zykova.anna.zao@gmail.com>
2021-02-21 18:00:20 +0200

    fixes for the main part

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,41 @@
 package tests;
 
+import io.qameta.allure.selenide.AllureSelenide;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static helpers.AttachmentsHelper.*;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+
+
 public class TestBase {
+    @BeforeAll
+    static void setup() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.startMaximized = true;
+
+        if(System.getProperty("remote_driver") != null) {
+            // config for Java + Selenide
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+            Configuration.remote = System.getProperty("remote_driver");
+        }
+    }
+
+    @AfterEach
+    public void afterEach() {
+        attachScreenshot("Last screenshot");
+        attachPageSource();
+        attachAsText("Browser console logs", getConsoleLogs());
+        if(System.getProperty("video_storage") != null)
+            attachVideo();
+        closeWebDriver();
+    }
 }
